60d99b67d7dfda8f147c5bfc5f9f09384dbb26bc
Theovillus <brayzso416@gmail.com>
2026-02-13 00:42:21 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_116320_TheovillusRoringkon/week01/CircleCalculator.kt
+++ src/oop_116320_TheovillusRoringkon/week01/CircleCalculator.kt
@@ -3,12 +3,14 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 fun main() {
     val radius = 7.0
     val pi = 3.14
-
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-
     println(checkSize(area))
 }
 
-fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle
+fun checkSize(area: Double) = if (area > 100) {
+    "This is a Big Circle"
+} else {
+    "This is a Small Circle"
+}
