8680641a3ca1be7a0b2791bc186ec06ff7b4fbe4
Theovillus <brayzso416@gmail.com>
2026-02-13 15:26:02 +0700

    week02(tugas1): create Loan class with default args

---
+++ src/oop_116320_TheovillusRoringkon/week02/PinjamBuku.kt
@@ -0,0 +1,15 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
