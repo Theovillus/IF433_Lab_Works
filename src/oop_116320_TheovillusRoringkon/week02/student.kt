f204dfb0c2b0c8fdc471053f4cea0c578d35281d
Theovillus <brayzso416@gmail.com>
2026-02-13 14:10:54 +0700

    week02: add validation in init block

--- src/oop_116320_TheovillusRoringkon/week02/student.kt
+++ src/oop_116320_TheovillusRoringkon/week02/student.kt
@@ -1,12 +1,16 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week02
 
-class student (val name : String, val nim : String, var major : String)
-    { init {
-        if (nim.length !=5) {
-            println("WARNING Objek tercipta dengan NIM ($nim) yang tidak valid!")
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String
+){
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di memory.")
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
 }
