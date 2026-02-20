09829dd21b13c645b173a66b23b7bc03f8b19605
Theovillus <brayzso416@gmail.com>
2026-02-20 14:01:04 +0700

    week03: fix setter using backing field

--- src/oop_116320_TheovillusRoringkon/week03/Employee.kt
+++ src/oop_116320_TheovillusRoringkon/week03/Employee.kt
@@ -1,5 +1,13 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week03
 
-class Employee (val name: String) {
+class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Error gaji tidak boleh negatif Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
+        }
 }
--- src/oop_116320_TheovillusRoringkon/week03/main.kt
+++ src/oop_116320_TheovillusRoringkon/week03/main.kt
@@ -1,2 +1,7 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week03
 
+fun main () {
+    val e = Employee("Budi")
+    println("---Test Error---")
+    e.salary = 5000
+}
