f609ba14cefc122e6cb9acb351674a7e836825fb
Theovillus <brayzso416@gmail.com>
2026-02-20 15:03:16 +0700

    week03: test all encapsolution logic in main

--- src/oop_116320_TheovillusRoringkon/week03/main.kt
+++ src/oop_116320_TheovillusRoringkon/week03/main.kt
@@ -2,6 +2,12 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 
 fun main () {
     val e = Employee("Budi")
-    println("---Test Error---")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+    println("Pajak yang harus dibayar: ${e.salary}")
+
 }
