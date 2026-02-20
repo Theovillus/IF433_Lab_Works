24c139c5c0bfefac2b13e386244baa06fab55439
Theovillus <brayzso416@gmail.com>
2026-02-20 15:28:48 +0700

    week03(tugas1):  create weapon class with validation

--- src/oop_116320_TheovillusRoringkon/week03/MainWeapon.kt
+++ src/oop_116320_TheovillusRoringkon/week03/MainWeapon.kt
@@ -1,13 +1,18 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week03
 
 fun main() {
-    val weapon = Weapon("Dragon Slayer")
 
-    weapon.damage = -50
-    println("Damage sekarang: ${weapon.damage}")
+    print("Masukkan nama senjata: ")
+    val weaponName = readLine() ?: "Unknown"
 
-    weapon.damage = 9999
-    println("Damage sekarang: ${weapon.damage}")
+    val weapon = Weapon(weaponName)
 
+    print("Masukkan damage: ")
+    val dmgInput = readLine()?.toIntOrNull() ?: 0
+
+    weapon.damage = dmgInput
+
+    println("Nama: ${weapon.name}")
+    println("Damage: ${weapon.damage}")
     println("Tier: ${weapon.tier}")
 }
