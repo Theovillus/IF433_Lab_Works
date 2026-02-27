fd8f16fffa6a760d33699b6e81f0daa70719e633
Theovillus <brayzso416@gmail.com>
2026-02-27 13:05:55 +0700

    week04: create Car class extending vehicle

--- src/oop_116320_TheovillusRoringkon/week04/Car.kt
+++ src/oop_116320_TheovillusRoringkon/week04/Car.kt
@@ -1,2 +1,9 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week04
 
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+
+}
