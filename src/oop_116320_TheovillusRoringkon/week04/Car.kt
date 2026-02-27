afa609fc7b1336f9f1060a200546588d0b202192
Theovillus <brayzso416@gmail.com>
2026-02-27 15:08:45 +0700

    week04: implement super keyword in Car accelerate

--- src/oop_116320_TheovillusRoringkon/week04/Car.kt
+++ src/oop_116320_TheovillusRoringkon/week04/Car.kt
@@ -5,10 +5,12 @@ open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
     override fun honk() {
         println("TIN TIN! Mobil $brand lewat!")
     }
+
     override fun accelerate() {
         super.accelerate() // Memanggil logika penambahan kecepatan milik Parent
         println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
     }
+
     fun openTrunk() {
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
     }
