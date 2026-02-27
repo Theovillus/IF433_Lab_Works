b4c1ce7871a207f5f2bd37854da3a6cab8f30a0c
Theovillus <brayzso416@gmail.com>
2026-02-27 15:07:58 +0700

    week04: override

--- src/oop_116320_TheovillusRoringkon/week04/Car.kt
+++ src/oop_116320_TheovillusRoringkon/week04/Car.kt
@@ -2,8 +2,14 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 
 open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
 
+    override fun honk() {
+        println("TIN TIN! Mobil $brand lewat!")
+    }
+    override fun accelerate() {
+        super.accelerate() // Memanggil logika penambahan kecepatan milik Parent
+        println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
+    }
     fun openTrunk() {
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
     }
-
 }
