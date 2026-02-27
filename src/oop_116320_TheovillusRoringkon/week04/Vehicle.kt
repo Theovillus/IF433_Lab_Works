6d0d112b60ad525e369e40347bec38c2081da209
Theovillus <brayzso416@gmail.com>
2026-02-27 13:02:37 +0700

    week04: add calculateStatus function

---
+++ src/oop_116320_TheovillusRoringkon/week04/Vehicle.kt
@@ -0,0 +1,13 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week04
+
+open class Vehicle (val brand: String) {
+    var speed = Int = 0
+
+    open fun accelerate() {
+        speed+= 10
+        println("$brand melaju. Kecepatan = $speed km/jam")
+    }
+    open fun honk () {
+        println("Beep beep!")
+    }
+}
