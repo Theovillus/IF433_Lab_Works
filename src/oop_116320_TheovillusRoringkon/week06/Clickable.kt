890d77b6d4085917bec36b93724e04ccc60f6695
Theovillus <brayzso416@gmail.com>
2026-03-18 22:14:32 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_116320_TheovillusRoringkon/week06/Clickable.kt
+++ src/oop_116320_TheovillusRoringkon/week06/Clickable.kt
@@ -1,7 +1,6 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week06
 
 interface Clickable {
-    val name : String = "Tombol Rahasia"
-
+    val name : String
     fun click()
 }
