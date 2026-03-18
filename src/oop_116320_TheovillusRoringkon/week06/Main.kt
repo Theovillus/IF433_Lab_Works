eef8dc0df31932e517bfb96fcfc9bf51aa36c1a2
Theovillus <brayzso416@gmail.com>
2026-03-18 23:00:00 +0700

    week06: (task): test smart home system logic in main

--- src/oop_116320_TheovillusRoringkon/week06/Main.kt
+++ src/oop_116320_TheovillusRoringkon/week06/Main.kt
@@ -20,7 +20,19 @@ fun main() {
     processCheckout(method = pay2, amount = 150000.0)
 
 
-        val lamp = SmartLamp("1", "Ruang Tamu")
-        val speaker = SmartSpeaker("2", "Google Nest Dapur")
-        val cctv = SmartCCTV("3", "Ezviz Garasi")
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("=== SECURITY MODE AKTIF ===")
+    hub.activateSecurityMode()
+
+    println("\n=== MATIKAN SEMUA PERANGKAT ===")
+    hub.turnOffAllSwitches()
 }
