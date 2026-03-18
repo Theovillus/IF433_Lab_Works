fd406a4793cb703a2bcc3b80f5ff220f84fc9f68
Theovillus <brayzso416@gmail.com>
2026-03-18 22:17:46 +0700

    week06: create Button class implementing Clickable

--- src/oop_116320_TheovillusRoringkon/week06/Clickable.kt
+++ src/oop_116320_TheovillusRoringkon/week06/Clickable.kt
@@ -3,4 +3,10 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 interface Clickable {
     val name : String
     fun click()
+
+    class Button (override val name: String) : Clickable {
+        override fun click() {
+            println("Tombol '$name' berhasil di click")
+        }
+    }
 }
