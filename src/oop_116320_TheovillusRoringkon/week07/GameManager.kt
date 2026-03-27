17c123718e0a7c2caaa9cc5bd4f15379fa049fc9
Theovillus <brayzso416@gmail.com>
2026-03-27 16:00:43 +0700

    week07:(task) (task) create GameItem data class

--- src/oop_116320_TheovillusRoringkon/week07/GameManager.kt
+++ src/oop_116320_TheovillusRoringkon/week07/GameManager.kt
@@ -1,4 +1,14 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week07
 
-class GameManager {
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
 }
