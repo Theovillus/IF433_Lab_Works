93667cc8e870116bcf1df0866445f2a710cc22a1
Theovillus <brayzso416@gmail.com>
2026-03-27 15:29:46 +0700

    week07:test Singleton and Companion Object in main

--- src/oop_116320_TheovillusRoringkon/week07/Main.kt
+++ src/oop_116320_TheovillusRoringkon/week07/Main.kt
@@ -1,4 +1,12 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week07
 
-class Main {
+fun main() {
+
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
--- src/oop_116320_TheovillusRoringkon/week07/NetworkClient.kt
+++ src/oop_116320_TheovillusRoringkon/week07/NetworkClient.kt
@@ -1,2 +1,17 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week07
 
+class NetworkClient private constructor (val url: String) {
+
+    fun connect() {
+        println("Connecting to $url...")
+    }
+
+    companion object {
+        const val BASE_URL = "https://api.umn.ac.id"
+
+        fun createClient(): NetworkClient {
+            println("Membangun NetworkClient dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
+}
