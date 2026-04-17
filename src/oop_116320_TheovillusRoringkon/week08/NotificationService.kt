9884dd3e8858b5f625816bc5059c7846c2edce6c
Theovillus <brayzso416@gmail.com>
2026-04-17 12:38:38 +0700

    week08: implement safe call with let block

--- src/oop_116320_TheovillusRoringkon/week08/NotificationService.kt
+++ src/oop_116320_TheovillusRoringkon/week08/NotificationService.kt
@@ -1,2 +1,18 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week08
 
+class NotificationService {
+
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+
+        if (user.email != null) {
+
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
--- src/oop_116320_TheovillusRoringkon/week08/OrderModels.kt
+++ src/oop_116320_TheovillusRoringkon/week08/OrderModels.kt
@@ -1,4 +1,6 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week08
 
-class OrderModels {
-}
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
