c8bc35508d3f914406c8cbe39306b8b892eae012
Theovillus <brayzso416@gmail.com>
2026-04-24 10:39:44 +0700

    week09:(task)create TradeLog data class

---
+++ src/oop_116320_TheovillusRoringkon/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
