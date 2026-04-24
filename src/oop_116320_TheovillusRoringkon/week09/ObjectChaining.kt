eaeb64a33319a024f9c9883504bb02ae54dae095
Theovillus <brayzso416@gmail.com>
2026-04-24 10:35:24 +0700

    week09:create Student data class and list

---
+++ src/oop_116320_TheovillusRoringkon/week09/ObjectChaining.kt
@@ -0,0 +1,12 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week09
+
+data class Student(val name: String, val gpa: Double)
+
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.9),
+        Student("Citra", 2.8),
+        Student("Dewi", 3.7)
+    )
+}
