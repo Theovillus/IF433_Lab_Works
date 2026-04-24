fb24e420723c3ef3e849af6de97484701388df59
Theovillus <brayzso416@gmail.com>
2026-04-24 10:47:08 +0700

    week09:(task) print top performers using forEach

--- src/oop_116320_TheovillusRoringkon/week09/TradingDashboard.kt
+++ src/oop_116320_TheovillusRoringkon/week09/TradingDashboard.kt
@@ -30,4 +30,10 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+
+    topPerformersString.forEach {
+        println(it)
+    }
 }
