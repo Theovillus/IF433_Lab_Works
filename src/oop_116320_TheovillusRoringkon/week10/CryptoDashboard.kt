bc45e28e29a4a8be0d02c3e1480f3307b2346a22
Theovillus <brayzso416@gmail.com>
2026-05-08 09:25:52 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_116320_TheovillusRoringkon/week10/CryptoDashboard.kt
+++ src/oop_116320_TheovillusRoringkon/week10/CryptoDashboard.kt
@@ -14,4 +14,12 @@ fun main() {
         println("Coin: ${coin.name}, Balance: ${coin.balance}")
     }
 
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 0.05))
+    txRepo.add(Transaction("TX002", 500.0))
+
+    println("\n--- Transaction History ---")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id}, Amount: ${tx.amount}")
+    }
 }
