cb0a50cb2722328e7822cc078ab7d7acdb40b317
Theovillus <brayzso416@gmail.com>
2026-05-08 11:49:58 +0700

    week11: implement apply for object initialization

--- src/oop_116320_TheovillusRoringkon/week11/ScopeConfiguration.kt
+++ src/oop_116320_TheovillusRoringkon/week11/ScopeConfiguration.kt
@@ -1,3 +1,11 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week11
 
 data class User(var name: String = "", var age: Int = 0)
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+}
