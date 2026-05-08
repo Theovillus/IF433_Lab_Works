047620a0cd71abe77ff2a94b40a19e83f56a84a9
Theovillus <brayzso416@gmail.com>
2026-05-08 09:27:46 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_116320_TheovillusRoringkon/week10/WalletModels.kt
+++ src/oop_116320_TheovillusRoringkon/week10/WalletModels.kt
@@ -1,4 +1,4 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week10
 
-data class Coin(val name: String, val balance: Double).
-data class Transaction(val id: String, val amount: Double).
+data class Coin(val name: String, val balance: Double)
+data class Transaction(val id: String, val amount: Double)
