930569227e4a827cf810f3efbb71e73239fe6c72
Theovillus <brayzso416@gmail.com>
2026-05-08 09:07:34 +0700

    week10:test math operations and comparable constraints

--- src/oop_116320_TheovillusRoringkon/week10/Main.kt
+++ src/oop_116320_TheovillusRoringkon/week10/Main.kt
@@ -1,5 +1,7 @@
 package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week10
 
+import sun.management.MemoryUsageCompositeData.getMax
+
 fun main(processData: (String) -> Unit) {
     println("=== TEST GENERIC CLASS ===")
         val intBox = Box(100)
@@ -16,4 +18,9 @@ fun main(processData: (String) -> Unit) {
         printData(3.14)
         val result = processData("stable coin")
         println("Hasil Proses : $result")
+
+        println("\n=== TEST CONSTRAINTS ===")
+        val math = MathBox(10.5, 20)
+        println("Total: ${math.sum()}")
+        println("Terbesar: ${ getMax (45, 90)}")
 }
