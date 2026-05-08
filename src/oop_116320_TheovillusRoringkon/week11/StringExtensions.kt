76ed1770add9deca5417b6f020cd1cb344b68e62
Theovillus <brayzso416@gmail.com>
2026-05-08 11:42:29 +0700

    week11: create string extension functions

---
+++ src/oop_116320_TheovillusRoringkon/week11/StringExtensions.kt
@@ -0,0 +1,8 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
