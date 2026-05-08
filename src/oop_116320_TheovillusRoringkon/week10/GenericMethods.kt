16beec69e62de6862a9f302d770d566e2a09ba04
Theovillus <brayzso416@gmail.com>
2026-05-08 08:52:35 +0700

    week10: implement generic function with return type

--- src/oop_116320_TheovillusRoringkon/week10/GenericMethods.kt
+++ src/oop_116320_TheovillusRoringkon/week10/GenericMethods.kt
@@ -2,4 +2,8 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 
 fun<T> printData(data: T){
     println ("Data yang diterima:$data")
+
+    fun <T> processData(input: T): T {
+        return input
+    }
 }
