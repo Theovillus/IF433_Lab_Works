6e2d502bea405d0799bb46f109a44b272f41e336
Theovillus <brayzso416@gmail.com>
2026-05-08 09:23:54 +0700

    week10: (task) populate coin repository

--- src/oop_116320_TheovillusRoringkon/week10/CryptoDashboard.kt
+++ src/oop_116320_TheovillusRoringkon/week10/CryptoDashboard.kt
@@ -2,4 +2,10 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+
+
 }
