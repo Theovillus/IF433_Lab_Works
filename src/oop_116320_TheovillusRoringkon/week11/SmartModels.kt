ee66334be81ede9e96b10b820361d29f4b5c6053
Theovillus <brayzso416@gmail.com>
2026-05-08 11:56:34 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_116320_TheovillusRoringkon/week11/SmartModels.kt
@@ -0,0 +1,8 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
