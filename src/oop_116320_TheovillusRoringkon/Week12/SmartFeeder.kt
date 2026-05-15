046a3c8ec469facb3930232c67b5e8510124e20c
Theovillus <brayzso416@gmail.com>
2026-05-15 13:56:22 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
+++ src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
@@ -14,4 +14,18 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 }
 fun main() {
     var currentKibbleStock = 50
+    try {
+        println("\n--- JADWAL MAKAN 1 (Pagi) ---")
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException) {
+        println("Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Error Umum: ${e.message}")
+    } finally {
 }
