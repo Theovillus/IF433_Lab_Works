1af4ed1e6dc68119d013ade379908dddfc9c2b6c
Theovillus <brayzso416@gmail.com>
2026-05-15 13:51:14 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
@@ -0,0 +1,5 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.Week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
