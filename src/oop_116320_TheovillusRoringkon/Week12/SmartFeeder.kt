029f17a4c7b75f45a2682b0f9cb88325ed350012
Theovillus <brayzso416@gmail.com>
2026-05-15 13:52:33 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
+++ src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
@@ -2,8 +2,14 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.Wee
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-    g
+
     if (isJammed) {
         throw DispenserJamException()
     }
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
