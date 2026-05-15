0c5e99ac455f45a7373bc85d0f4ae5d37ef405a6
Theovillus <brayzso416@gmail.com>
2026-05-15 13:51:57 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
+++ src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
@@ -2,4 +2,8 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.Wee
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    g
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
