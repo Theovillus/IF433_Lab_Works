c4ec5f7bec4a8e92554fa408acf7d12eecc7ba01
Theovillus <brayzso416@gmail.com>
2026-05-15 13:58:58 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
+++ src/oop_116320_TheovillusRoringkon/Week12/SmartFeeder.kt
@@ -36,4 +36,8 @@ fun main() {
     val result = runCatching {
         dispenseKibble(requestedGram = 30, availableGram = currentKibbleStock, isJammed = false)
     }
+    result.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+    }
 }
