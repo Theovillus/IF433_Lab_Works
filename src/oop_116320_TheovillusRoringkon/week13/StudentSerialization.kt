50e5dfde97c34ee5edb3777ea3c325627cec9af3
Theovillus <brayzso416@gmail.com>
2026-05-22 13:38:14 +0700

    week13: create Student model

---
+++ src/oop_116320_TheovillusRoringkon/week13/StudentSerialization.kt
@@ -0,0 +1,6 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)
