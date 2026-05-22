0c321920fec62d2d78fab0197d2345919f7a4779
Theovillus <brayzso416@gmail.com>
2026-05-22 13:43:33 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_116320_TheovillusRoringkon/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
