dde47cf858287429d12732fbace90f879d86bd98
Theovillus <brayzso416@gmail.com>
2026-05-29 12:13:17 +0700

    week14: segregate device interfaces

---
+++ src/oop_116320_TheovillusRoringkon/week14/IspRefactored.kt
@@ -0,0 +1,9 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week14
+
+interface Printable {
+    fun print(doc: String)
+}
+
+interface Scannable {
+    fun scan(doc: String): String
+}
