fc440caa93d855147d3e4e5ef04dcac584c045ab
Theovillus <brayzso416@gmail.com>
2026-05-29 12:11:26 +0700

    week14: implement Shape independently to fix LSP

--- src/oop_116320_TheovillusRoringkon/week14/LspRefactored.kt
+++ src/oop_116320_TheovillusRoringkon/week14/LspRefactored.kt
@@ -3,3 +3,10 @@ package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.wee
 interface Shape {
     fun area(): Int
 }
+class SafeRectangle(var width: Int, var height: Int) : Shape {
+    override fun area() = width * height
+}
+
+class SafeSquare(var side: Int) : Shape {
+    override fun area() = side * side
+}
