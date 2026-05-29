72e0d03688a21983737903d0b1bc9e821c898055
Theovillus <brayzso416@gmail.com>
2026-05-29 12:15:57 +0700

    week14: create Database abstraction and implementations

---
+++ src/oop_116320_TheovillusRoringkon/week14/DipRefactored.kt
@@ -0,0 +1,9 @@
+package oop_116320_TheovillusRoringkon.week01.oop_116320_TheovillusRoringkon.week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase : Database {
+    override fun query(sql: String): List<String> = listOf("pg_data1", "pg_data2")
+}
